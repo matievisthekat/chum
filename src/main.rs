dba558cd4beab6c5c3723cf563fee3fed9a93ca1
matievisthekat <matievisthekat@gmail.com>
2021-11-10 21:00:11 +0200

    remove cli parsing from main

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-use std::collections::HashMap;
-use std::env;
-
 fn main() {
-    const FLAG_IDENTIFIER: &str = "--";
-    let args: Vec<String> = env::args().skip(1).collect();
-    let command = args[0].clone().to_lowercase();
-
-    let flag_args: Vec<String> = args
-        .iter()
-        .filter(|arg| arg.starts_with(FLAG_IDENTIFIER))
-        .map(|arg| arg.to_string())
-        .collect();
-
-    let mut flags = HashMap::new();
-    for flag in flag_args {
-        let mut flag_name = flag.clone();
-        for _ in FLAG_IDENTIFIER.chars() {
-            flag_name.remove(0);
-        }
-        flags.insert(flag_name, true);
-    }
+    const FLAG_IDENTIFIER: &str = ".";
 }
