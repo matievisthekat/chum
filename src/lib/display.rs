fa8044ea4dbc31af9d77086f9a76caa71e4eb9ea
matievisthekat <matievisthekat@gmail.com>
2021-11-12 11:21:23 +0200

    allow dead code for coloured prints

--- src/lib/display.rs
+++ src/lib/display.rs
@@ -1,18 +1,22 @@
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
+#[allow(dead_code)]
 pub fn success(msg: &str) {
   writeln(Color::Green, &("\u{2714} ".to_owned() + msg));
 }
 
+#[allow(dead_code)]
 pub fn warn(msg: &str) {
   writeln(Color::Yellow, &("\u{26A0} ".to_owned() + msg));
 }
 
+#[allow(dead_code)]
 pub fn error(msg: &str) {
   writeln(Color::Red, &("\u{2718} ".to_owned() + msg));
 }
 
+#[allow(dead_code)]
 pub fn info(msg: &str) {
   writeln(Color::Blue, &("\u{2139} ".to_owned() + msg));
 }
