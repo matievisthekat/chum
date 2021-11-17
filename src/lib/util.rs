a724408f5bc6bd3927796e814f0bb93fe16e147d
matievisthekat <matievisthekat@gmail.com>
2021-11-17 09:44:20 +0200

    add read_file_to_string func

--- src/lib/util.rs
+++ src/lib/util.rs
@@ -10,6 +10,36 @@ pub fn get_full_path(p: PathBuf) -> PathBuf {
   }
 }
 
+pub fn read_file_to_string(p: &PathBuf) -> Result<String, (i32, String)> {
+  let content_result = fs::read(&p);
+  match content_result {
+    Ok(content_bytes) => {
+      let string_result = String::from_utf8(content_bytes);
+      match string_result {
+        Ok(string) => {
+          return Ok(string);
+        }
+        Err(e) => {
+          return Err((
+            1,
+            format!(
+              "Failed to parse {} content from bytes to string: {}",
+              p.display(),
+              e
+            ),
+          ));
+        }
+      }
+    }
+    Err(e) => {
+      return Err((
+        1,
+        format!("Failed to read the contents of {}: {}", p.display(), e),
+      ));
+    }
+  }
+}
+
 pub fn read_dir_recursive(p: &Path) -> Result<Vec<PathBuf>, String> {
   let mut all_files = vec![];
 
